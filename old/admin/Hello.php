24b7f379e9b1b8f5a6eb5e1d77a8b3b03a7dcacc
Anton Zaika <antonzaika1993@gmail.com>
2021-09-29 18:52:02 +0300

    refactoring for old templates

--- old/admin/Hello.php
+++ old/admin/Hello.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Embed API Demo</title>
-<script>
-(function(w,d,s,g,js,fs){
-  g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
-  js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
-  js.src='https://apis.google.com/js/platform.js';
-  fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
-}(window,document,'script'));
-</script>
-</head>
-<body>
+  <head>
+    <title>Embed API Demo</title>
+    <script>
+    (function(w,d,s,g,js,fs){
+      g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
+      js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
+      js.src='https://apis.google.com/js/platform.js';
+      fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
+    }(window,document,'script'));
+    </script>
+  </head>
+  <body>
 
 <header>
   <div id="embed-api-auth-container"></div>
